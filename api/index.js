e0dc835268b843497f182cab350138610246a71b
thelokys <ricardo.guidetti@gmail.comexit>
2020-04-11 16:14:54 -0300

    added api

---
+++ api/index.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const URL = require('url')
+const fs = require('fs')
+const path = require('path')
+
+const data = require('./urls.json')
+
+function writeFile(callback){
+  fs.writeFile(
+    path.join(__dirname, "urls.json"), 
+    JSON.stringify(data, null, 2),
+    (err) => {
+      if(err) throw err;
+
+      callback(JSON.stringify({ message: "ok" }))
+    }
+  )
+}
+
+http.createServer( (req, res) => {
+  const { name, url, del } =  URL.parse(req.url, true).query
+  
+  res.setHeader('Content-Type','application/json')
+
+  res.writeHead(200, {
+    'Access-Controll-Allow-Origin': '*'
+  })
+  res.write
+
+  if(!name || !url)
+    return res.end(JSON.stringify(data))
+
+  if (del) {
+    data.urls = data.urls.filter(item => String(item.url) !== String(url))
+    return writeFile((message) => res.end(message))
+  }
+  data.urls.push({name, url})
+  return writeFile((message) => res.end(message))
+
+}).listen(3000, () => console.log('Api is running'))
